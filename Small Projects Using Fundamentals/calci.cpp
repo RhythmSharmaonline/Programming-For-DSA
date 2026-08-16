8f80090c99504b2e3e4a4ffdc51b2bd655bce879
RhythmSharmaonline <rhythmsharmamac@gmail.com>
2026-08-16 22:55:55 +0530

    switch case pending

--- Small Projects Using Fundamentals/calci.cpp
+++ Small Projects Using Fundamentals/calci.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void welcome(){
-    cout<< "-----Welcome To our Calculator Services-----";
-    cout<< "<--Choose Your Preference-->";
-    cout<< "1.Addition";
-    cout<< "2.Substraction";
-    cout<< "3.Multiplication";
-    cout<< "4.Division";
+//Preference Page
+int welcome(){
+    int choice;
+    string menu = "-----Welcome To our Calculator Services-----\n"
+        "<--Choose Your Preference-->\n"
+        "1.Addition\n"
+        "2.Substraction\n"
+        "3.Multiplication\n"
+        "4.Division\n"
+        "Enter Your Preference: ";
+    cout <<menu;
+    cin>>choice;
+    return choice;
+}
+
+//Input function
+void inp(int &n1, int &n2){
+    cout<<"Enter First Number: ";
+    cin >> n1;
+    cout<<"Enter Second Number: ";
+    cin>> n2;
 }
 
 //Addition
@@ -17,32 +31,33 @@ int add(int n1,int n2){
 
 //Substraction
 int subs(int n1,int n2){
-    if (n1>n2){
-        return n1-n2;
-    }
-    else if(n2>n1){
-        return n2-n1;
-    }
-    else if (n1==n2){
-        return 0;
-    }
-    else {
-        cout<< "Enter Valid Input";
-    }
+    return n1-n2;
 }
 
 //Multiplication
+int multi(int n1, int n2){
+    return n1* n2;
+}
 
+//Division
+int divi(int n1, int n2){
+    if(n2 == 0){
+        cout << "Can't divide by 0 ";
+        return 0;
+    }
+    return n1/n2;
+}
 
+//MAIN Function
 int main() {
-    int n1;
-    int n2;
-    char op ;
-    cout<< "Enter First Number: "<<endl;
-    cin >> n1;
-    cout <<"Enter Second Number: "<<endl;
-    cin >>n2;
-    cout<<"Enter Operator: ";
-    cin>>op;
+    int choice = welcome();
+    int a,b ;
+    inp(a,b);
+    switch(choice){
+        case 1:
+            add(a,b);
+            
+    }
+
     return 0;
 }
