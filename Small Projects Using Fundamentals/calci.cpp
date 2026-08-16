963b132cd59e697437bb12c48ccbb8c52aaa17fa
RhythmSharmaonline <rhythmsharmamac@gmail.com>
2026-08-16 22:15:06 +0530

    calci structure mid

--- Small Projects Using Fundamentals/calci.cpp
+++ Small Projects Using Fundamentals/calci.cpp
@@ -1,14 +1,48 @@
 #include <iostream>
 using namespace std;
 
+void welcome(){
+    cout<< "-----Welcome To our Calculator Services-----";
+    cout<< "<--Choose Your Preference-->";
+    cout<< "1.Addition";
+    cout<< "2.Substraction";
+    cout<< "3.Multiplication";
+    cout<< "4.Division";
+}
+
+//Addition
+int add(int n1,int n2){
+    return n1+n2;
+}
+
+//Substraction
+int subs(int n1,int n2){
+    if (n1>n2){
+        return n1-n2;
+    }
+    else if(n2>n1){
+        return n2-n1;
+    }
+    else if (n1==n2){
+        return 0;
+    }
+    else {
+        cout<< "Enter Valid Input";
+    }
+}
+
+//Multiplication
+
+
 int main() {
     int n1;
     int n2;
-    string operator;
+    char op ;
     cout<< "Enter First Number: "<<endl;
     cin >> n1;
     cout <<"Enter Second Number: "<<endl;
     cin >>n2;
-
+    cout<<"Enter Operator: ";
+    cin>>op;
     return 0;
 }
