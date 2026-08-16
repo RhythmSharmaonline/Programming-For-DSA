bf22d7c54a449f3e66ade44a61d2fc1875f9b37d
RhythmSharmaonline <rhythmsharmamac@gmail.com>
2026-08-16 21:35:33 +0530

    Fundamentals

--- Basics Cpp/intro.cpp
+++ Basics Cpp/intro.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 using namespace std;
-int main(){
-    cout << "Hello" << endl;
+
+int main() {
+    cout <<"Welcome To Cpp Basics!!"<<endl;
+    int age;
+    string name;
+    cout<< "Enter Age: ";
+    cin>> age;
+    cin.ignore();
+    getline(cin,name);
+    cout <<"Age: "<<age;
+
+    return 0;
 }
