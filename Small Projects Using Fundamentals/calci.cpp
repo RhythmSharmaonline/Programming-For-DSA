05d04d6d75430c6787ebd42a2e63622fee6449d6
RhythmSharmaonline <rhythmsharmamac@gmail.com>
2026-08-16 23:11:54 +0530

    issue in switch case and loop

--- Small Projects Using Fundamentals/calci.cpp
+++ Small Projects Using Fundamentals/calci.cpp
@@ -4,14 +4,16 @@ using namespace std;
 //Preference Page
 int welcome(){
     int choice;
-    string menu = "-----Welcome To our Calculator Services-----\n"
-        "<--Choose Your Preference-->\n"
+    string menu = "-----Welcome To our Calculator Services-----\n \n"
+        "=====Calculator=====\n"
+        "<---Choose Your Preference--->\n"
         "1.Addition\n"
         "2.Substraction\n"
         "3.Multiplication\n"
         "4.Division\n"
+        "5.Exit\n"
         "Enter Your Preference: ";
-    cout <<menu;
+    cout<<menu;
     cin>>choice;
     return choice;
 }
@@ -40,24 +42,43 @@ int multi(int n1, int n2){
 }
 
 //Division
-int divi(int n1, int n2){
+float divi(int n1, int n2){
     if(n2 == 0){
         cout << "Can't divide by 0 ";
         return 0;
     }
-    return n1/n2;
+    return float(n1/n2);
 }
 
 //MAIN Function
 int main() {
-    int choice = welcome();
     int a,b ;
     inp(a,b);
-    switch(choice){
-        case 1:
-            add(a,b);
-            
-    }
+    while(true){
+        int choice = welcome();
+        switch(choice){
+            case 1:
+                cout<<"Result= "<< add(a,b);
+                break;
+            case 2: 
+                cout<<"Result= "<< subs(a,b);
+                break;
+            case 3:
+                cout<<"Result= "<< multi(a,b);
+                break;
+            case 4:
+                cout<<"Result= "<< divi(a,b);
+                break;
+            case 5:
+                cout<<"-----Thank You For Using our Services----";
+                return 0;
+            default:
+                cout <<"Invalid Choice";
+                break;
+
+                
+        }
+}
 
     return 0;
 }
