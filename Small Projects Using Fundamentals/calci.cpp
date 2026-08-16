afaa2bdff833bf87509d09ae61a13d0b6a23bbbd
RhythmSharmaonline <rhythmsharmamac@gmail.com>
2026-08-16 21:46:04 +0530

    Project For Cpp

---
+++ Small Projects Using Fundamentals/calci.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1;
+    int n2;
+    string operator;
+    cout<< "Enter First Number: "<<endl;
+    cin >> n1;
+    cout <<"Enter Second Number: "<<endl;
+    cin >>n2;
+
+    return 0;
+}
